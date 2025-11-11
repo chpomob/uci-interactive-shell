1606cd492e458fb589038c43fbfb9d08b96fa277
chpo <chpomob@gmail.com>
2025-11-11 14:03:58 +0100

    Modularize session command bridge

---
+++ include/uci_cmd_framework_session.h
@@ -0,0 +1,9 @@
+#ifndef UCI_CMD_FRAMEWORK_SESSION_H
+#define UCI_CMD_FRAMEWORK_SESSION_H
+
+#include "uci_command_framework.h"
+
+extern const uci_command_def_t g_uci_session_command_defs[];
+extern const int g_uci_session_command_defs_count;
+
+#endif /* UCI_CMD_FRAMEWORK_SESSION_H */
